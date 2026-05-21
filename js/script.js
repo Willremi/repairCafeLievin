35d2a9f826bc6bb37f5074da65944132296368ff
Rémi Willebrouck <willremi@gmail.com>
2026-05-21 10:37:51 +0200

    ajout du bouton de retour en haut et de son style

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
+const backToTop = document.getElementById("backToTop");
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("show");
+});
+
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        backToTop.style.display = "block";
+    } else {
+        backToTop.style.display = "none";
+    }
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
